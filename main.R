0d653747848203cb9dbf52e87df9e16604fb1360
MichaelStClair <mikestclair@gmail.com>
2017-08-19 18:24:31 -0400

    Add files via upload

--- main.R
+++ main.R
@@ -20,6 +20,71 @@ which(agricultureLocal)[1:3]
 
 # Question 2
 
+# Read the JPEG into R
+# install.packages("jpeg")
+library(jpeg)
+if(!file.exists("./data")){dir.create("./data")}
+destFile <- "./data/jeff.jpg"
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg"
+download.file(fileUrl,destfile=destFile,mode="wb")
+img <- readJPEG(destFile, native = TRUE)
+quantile(img, probs = c(0.3, 0.8))
+#       30%       80% 
+#  -15259150 -10575416 
+
+
+## Question 3
+
+# we are gonna use library(plyr)
+# install.packages("plyr")
+library(plyr)
+
+# Get GDP and EDU data
+if(!file.exists("./data")){dir.create("./data")}
+GDPfileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+EDUfileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+GDPdestFile <- "./data/gdp.csv"
+EDUdestFile <- "./data/edu.csv"
+download.file(GDPfileUrl,destfile=GDPdestFile)
+download.file(EDUfileUrl,destfile=EDUdestFile)
+
+# Load into data frames
+GDPdata <- read.csv(GDPdestFile)
+EDUdata <- read.csv(EDUdestFile)
+
+mergeData <- merge(x = GDPdata, y = EDUdata, by.x = "X", by.y = "CountryCode")
+
+head(arrange(mergeData,desc(Gross.domestic.product.2012)), n = 25)
+
+
+mergeData[1:13,]
+
+head(sort(mergeData$Gross.domestic.product.2012,decreasing=TRUE), n = 13)
+
+testData <- mergeData[order("Gross.domestic.product.2012", decreasing = TRUE)]
+
+testData$
+
+mergeData[1:13, order("Gross.domestic.product.2012", decreasing = TRUE)]
+
+
+sample(mergeData)
+
+
+head(GDPdata, n=3)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
